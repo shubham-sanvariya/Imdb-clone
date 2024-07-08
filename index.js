3214892bb56eb61f6e839cafb5bb51f0b586f6f2
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-07-08 12:42:23 +0530

    adding search movie display fn's and styling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const API_KEY = "bfd6b563";
 
 const search_inp_el = document.querySelector("#search_inp");
+const movies_content_el = document.querySelector(".movies-content");
 
 const searchMovies = async () => {
     const input_val = search_inp_el.value;
@@ -27,6 +28,28 @@ const searchMovies = async () => {
 
 search_inp_el.addEventListener("input", searchMovies);
 
-function displayMovies(){
-    // todo
+function displayMovies(movies){
+    movies_content_el.innerHTML = "";
+    movies.forEach(movie => {
+        const movie_el = createMovieEl(movie);
+        movies_content_el.append(movie_el);
+    });
+}
+
+function createMovieEl(movie){
+    const movieElem = document.createElement('div');
+    movieElem.classList.add('movie');
+
+    movieElem.innerHTML = `
+    <a href="movie.html?imdbID=${movie.imdbID}">
+      <img src="${movie.Poster}" alt="${movie.Title}">
+    </a>
+    <div class="movieText">
+      <h2 class="white">${movie.Title}</h2>
+      <p class="white">${movie.Year}</p>
+      <button>favorites</button>
+      </div>
+  `;
+
+  return movieElem;
 }
